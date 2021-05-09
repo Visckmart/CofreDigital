3edd5e18366ca27631a05cd0a8686372d2c06c9f
Visckmart <visckmart@hotmail.com>
2021-05-09 18:23:34 -0300

    Preparando a tabela de mensagens e ajustes no handler de banco de dados.

--- DatabaseHandler.java
+++ DatabaseHandler.java
@@ -2,8 +2,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Date;
 
 public class DatabaseHandler {
+    private static DatabaseHandler instance;
+    public static DatabaseHandler getInstance() {
+        if (instance == null) {
+            try {
+                instance = new DatabaseHandler();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return instance;
+    }
+
     Connection connection;
     public DatabaseHandler() throws Exception {
         Class.forName("org.sqlite.JDBC");
@@ -17,15 +31,28 @@ public class DatabaseHandler {
         statement.executeUpdate(query);
     }
 
-    public boolean verifyUserEmail(String email) throws Exception {
+    public UserState verifyUserEmail(String email) throws Exception {
         Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery("select * from person where email="+email+" and CURRENT_TIMESTAMP  > TIMEOUT;");
-        return rs.next();
+        ResultSet rs = statement.executeQuery(
+            "SELECT * from USUARIOS WHERE email = '" + email + "';"
+        );
+        if (rs.next()) {
+            Timestamp timestamp = rs.getTimestamp("timeout");
+            if (timestamp != null) {
+                Date date = new Date(timestamp.getTime());
+                if (date.compareTo(new Date()) > 0) {
+                    return UserState.BLOCKED;
+                }
+            }
+            return UserState.VALID;
+        } else {
+            return UserState.INVALID;
+        }
     }
 
     public String[] getPasswordAndSalt(String email) throws Exception {
         Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery("select * from person where email="+email);
+        ResultSet rs = statement.executeQuery("select * from usuarios where email='"+email+"'");
         if(rs.next()) {
             String password = rs.getString("senha");
             String salt = rs.getString("salt");
@@ -57,5 +84,12 @@ public class DatabaseHandler {
     public static void main(String[] args) throws Exception {
         DatabaseHandler handler = new DatabaseHandler();
         handler.registerUser("th@2132.com", "oi".getBytes(), "123", "232", 0);
+        // Statement statement = handler.connection.createStatement();
+        // ResultSet rs = statement.executeQuery("select * from mensagens");
+        // while(rs.next()) {
+        //     System.out.println(rs.getInt("codigo"));
+        //     System.out.println(rs.getString("mensagem"));
+        //     // System.out.printf("%s %s %s %d %d\n", rs.getString("email"), rs.getString("senha"), rs.getString("salt"), rs.getInt("attempts"), rs.getInt("gid"));
+        // }
     }
 }
