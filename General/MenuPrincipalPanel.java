cdad7e38d408b31eaacd2465f5b9d692f55b84da
Visckmart <visckmart@hotmail.com>
2021-05-15 19:38:20 -0300

    Escondendo cadastro dos usuários

--- General/MenuPrincipalPanel.java
+++ General/MenuPrincipalPanel.java
@@ -5,81 +5,78 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+import Authentication.UserGroup;
+import Authentication.UserState;
+import Utilities.FrameHandler;
 import Utilities.LogHandler;
 
 public class MenuPrincipalPanel extends GeneralPanel {
     
-      public MenuPrincipalPanel() {
+    public MenuPrincipalPanel() {
         super("Menu principal", false);
         prepararCorpo(210, 190, 280, 300);
         LogHandler.logWithUser(5001);
-      }
+    }
       
-      void prepararCorpo(int offsetX, int offsetY, int width, int height) {
-        JButton bt1 = new JButton("Cadastrar um novo usuário");
-        bt1.setFont(new Font(null, Font.PLAIN, 15));
-        bt1.setBounds(offsetX, offsetY, width, height/4 - 5);
-        bt1.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e) {
-              LogHandler.logWithUser(5002);
-              JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(bt1);
-              CadastroPanel cap = new CadastroPanel();
-              // cap.setFileList(IndexHandler.testIndexHandler());
-              frame.setContentPane(cap);
-              frame.invalidate();
-              frame.validate();
-          }
-        });
-        add(bt1);
+    void prepararCorpo(int offsetX, int offsetY, int width, int height) {
+        boolean mostrarCadastro = UserState.group == UserGroup.ADMIN;
+        int qtdBotoes = mostrarCadastro ? 4 : 3;
+        int index = 0;
+        int newYoffset;
+
+        if (mostrarCadastro) {
+            JButton bt1 = new JButton("Cadastrar um novo usuário");
+            bt1.setFont(new Font(null, Font.PLAIN, 15));
+            newYoffset = offsetY + (height/qtdBotoes) * index;
+            bt1.setBounds(offsetX, offsetY, width, height/qtdBotoes - 5);
+            bt1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    LogHandler.logWithUser(5002);
+                    FrameHandler.showPanel(new CadastroPanel());
+                }
+            });
+            add(bt1);
+            index += 1;
+        }
         
         JButton bt2 = new JButton("<html><p style='text-align:center;'>Alterar senha pessoal e<br>certificado digital do usuário</p.</html>");
         bt2.setFont(new Font(null, Font.PLAIN, 15));
-        bt2.setBounds(offsetX, offsetY + height/4, width, height/4 - 5);
+        newYoffset = offsetY + (height/qtdBotoes) * index;
+        bt2.setBounds(offsetX, newYoffset, width, height/qtdBotoes - 5);
         bt2.setHorizontalAlignment(SwingConstants.CENTER);
         bt2.addActionListener(new ActionListener() {
           public void actionPerformed(ActionEvent e) {
               LogHandler.logWithUser(5003);
-              JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(bt2);
-              AlterarCertificadoPanel cap = new AlterarCertificadoPanel();
-              // cap.setFileList(IndexHandler.testIndexHandler());
-              frame.setContentPane(cap);
-              frame.invalidate();
-              frame.validate();
+              FrameHandler.showPanel(new AlterarCertificadoPanel());
           }
         });
         add(bt2);
+        index += 1;
         
         JButton bt3 = new JButton("<html><p style='text-align:center;'>Consultar pasta de arquivos<br>secretos do usuário</p></html>");
-        bt3.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
-                LogHandler.logWithUser(5004);
-                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(bt3);
-                ConsultarArquivosPanel cap = new ConsultarArquivosPanel();
-                // cap.setFileList(IndexHandler.testIndexHandler());
-                frame.setContentPane(cap);
-                frame.invalidate();
-                frame.validate();
-              }
-            });
         bt3.setFont(new Font(null, Font.PLAIN, 15));
-        bt3.setBounds(offsetX, offsetY + (height/4) * 2, width, height/4 - 5);
+        newYoffset = offsetY + (height/qtdBotoes) * index;
+        bt3.setBounds(offsetX, newYoffset, width, height/qtdBotoes - 5);
+        bt3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                LogHandler.logWithUser(5004);
+                FrameHandler.showPanel(new ConsultarArquivosPanel());
+            }
+        });
         add(bt3);
+        index += 1;
 
         JButton bt4 = new JButton("Sair do Sistema");
         bt4.setFont(new Font(null, Font.PLAIN, 15));
-        bt4.setBounds(offsetX, offsetY + (height/4) * 3, width, height/4 - 5);
+        newYoffset = offsetY + (height/qtdBotoes) * index;
+        bt4.setBounds(offsetX, newYoffset, width, height/qtdBotoes - 5);
         bt4.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e) {
                 LogHandler.logWithUser(5005);
                 LogHandler.logWithUser(9003);
-                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(bt4);
-                ConfirmacaoSaidaPanel cap = new ConfirmacaoSaidaPanel();
-                // cap.setFileList(IndexHandler.testIndexHandler());
-                frame.setContentPane(cap);
-                frame.invalidate();
-                frame.validate();
-              }
-            });
+                FrameHandler.showPanel(new ConfirmacaoSaidaPanel());
+            }
+        });
         add(bt4);
       }
   }
